24bfe1b97cc18506809b9f8b8fb2d20c98dd54a5
Liam <admin@fascinated.cc>
2022-11-17 16:42:35 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Current kwh cost
+
+<p>This was pretty much made for me, since I live in the UK.</p>
+<p>Don't expect this to work for you</p>
---
+++ index.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+import express from "express";
+import moment from "moment/moment.js";
+
+moment.locale("en-GB");
+
+const app = express();
+
+function getCurrentDate() {
+	return moment().format("l").replaceAll("/", "-");
+}
+
+async function fetchCurrentKwh() {
+	const url = `https://odegdcpnma.execute-api.eu-west-2.amazonaws.com/development/prices?dno=19&voltage=LV&start=${getCurrentDate()}&end=${getCurrentDate()}`;
+	console.log(url);
+	const response = await axios.get(url);
+	const json = response.data;
+	const baseCost = json.data.data[0].Overall;
+	let cost = baseCost * 2.2;
+	const currentHour = new Date().getHours();
+	if (currentHour > 16 && currentHour < 19) {
+		cost += 12;
+	}
+	cost *= 1.2;
+	return cost;
+}
+
+app.get("/", async (req, res) => {
+	const currentCost = await fetchCurrentKwh();
+	res.status(200).json({ cost: currentCost });
+});
+
+app.listen(3000, () => {
+	console.log("Started on http://localhost:3000");
+});
